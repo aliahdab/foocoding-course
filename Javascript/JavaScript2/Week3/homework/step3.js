8a2a780e488a65439264e7cc5982bb3e9a61243a
Ali Ahdab <aliahdab@gmail.com>
2019-09-14 21:46:37 +0200

    solve homework's week3

--- Javascript/JavaScript2/Week3/homework/step3.js
+++ Javascript/JavaScript2/Week3/homework/step3.js
@@ -6,9 +6,7 @@ function createBase(base) {
 
 const addSix = createBase(6);
 
-// eslint-disable-next-line no-console
 console.log(addSix(10)); // returns 16
-// eslint-disable-next-line no-console
 console.log(addSix(21)); // returns 27
 
 // Do not change or remove anything below this line
