76cadac02ea7cad3730d37242e778fa5dd025b3b
Ali Ahdab <aliahdab@gmail.com>
2019-09-29 09:40:28 +0200

    before css

--- Javascript/JavaScript3_/homework/fetch-api/index.js
+++ Javascript/JavaScript3_/homework/fetch-api/index.js
@@ -14,9 +14,11 @@ const getData = async (url) => {
 const generateContributions = async (element) => {
   const anArray = await getData(element.contributors_url);
   const rightDiv = document.createElement('div');
+  rightDiv.setAttribute('id', 'rightDiv');
+  const paragraph = document.createElement('p');
+  paragraph.setAttribute('id', 'paragraph');
   anArray.forEach((ele) => {
-    const paragraph = document.createElement('p');
-    paragraph.setAttribute('id', 'rightDiv');
+
     const uList = document.createElement('ul');
     uList.setAttribute('id', 'ul');
     const liList = document.createElement('li');
@@ -28,11 +30,13 @@ const generateContributions = async (element) => {
     liList.append(a);
     const img = document.createElement('img');
     img.setAttribute('src', ele.avatar_url);
+    img.setAttribute('id', 'ul');
+
     liList.append(img)
     paragraph.append(uList);
-    rightDiv.append(paragraph)
+
   })
-  return rightDiv
+  return paragraph
 }
 
 const generateDepoDetails = (element) => {
@@ -57,10 +61,13 @@ const generateDepoList = async () => {
   const aArray = await getData('https://api.github.com/orgs/foocoding/repos?per_page=100');
   const myArray = customizingTheArray(aArray);
   const root = document.getElementById('root');
+  let leftDiv = document.createElement('div');
+  leftDiv.setAttribute('id', 'leftDiv');
   let rightDiv = document.createElement('div');
+  rightDiv.setAttribute('id', 'rightDiv2');
   const uList = document.createElement('ul');
-  const details = document.createElement('div');
-
+  let details = document.createElement('div');
+  details.setAttribute('id', 'details');
   let str;
   uList.setAttribute('id', 'mainList');
   myArray.forEach(element => {
@@ -71,21 +78,20 @@ const generateDepoList = async () => {
     btn.appendChild(document.createTextNode(str))
     btn.setAttribute('id', str);
     uList.appendChild(liList)
-    root.appendChild(uList);
-
+    leftDiv.appendChild(uList);
     btn.addEventListener('click', async () => {
-      details.innerHTML = '';
-      rightDiv.innerHTML = '';
-      details.appendChild(generateDepoDetails(element));
-      root.append(details);
+      details.remove();
+      rightDiv.remove();
+      details = generateDepoDetails(element);
       rightDiv = await generateContributions(element);
+      root.append(details);
       root.append(rightDiv);
     });
   });
+  root.append(leftDiv)
 }
 
 generateDepoList();
-
 const customizingTheArray = (anArray) => {
   let myArray = [];
   anArray.map(item => {
