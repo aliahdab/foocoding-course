1a53f89d06d60581cbd473efdec638c15c3ed2eb
Ali Ahdab <aliahdab@gmail.com>
2019-10-05 11:36:33 +0200

    editing css

--- Javascript/JavaScript3_/homework/fetch-api/index.js
+++ Javascript/JavaScript3_/homework/fetch-api/index.js
@@ -1,24 +1,22 @@
-'use strict'
+'use strict';
 
-const getData = async (url) => {
+const getData = async url => {
   try {
-    let res = await fetch(url)
+    let res = await fetch(url);
     let data = await res.json();
-    //console.log(data);
-    return data
+    return data;
   } catch (err) {
-    alert(err)
+    alert(err);
   }
-}
+};
 
-const generateContributions = async (element) => {
+const generateContributions = async element => {
   const anArray = await getData(element.contributors_url);
   const rightDiv = document.createElement('div');
   rightDiv.setAttribute('id', 'rightDiv');
   const paragraph = document.createElement('p');
   paragraph.setAttribute('id', 'paragraph');
-  anArray.forEach((ele) => {
-
+  anArray.forEach(ele => {
     const uList = document.createElement('ul');
     uList.setAttribute('id', 'ul');
     const liList = document.createElement('li');
@@ -26,36 +24,38 @@ const generateContributions = async (element) => {
     uList.append(liList);
     const a = document.createElement('a');
     a.href = ele['html_url'];
-    a.textContent = ele.login
-    liList.append(a);
+    a.textContent = ele.login;
+    (a.target = '_blank'), liList.append(a);
     const img = document.createElement('img');
     img.setAttribute('src', ele.avatar_url);
     img.setAttribute('alt', 'Repo Image');
-    //  a.appendChild(img);
-    liList.append(img)
+    liList.append(img);
     paragraph.append(uList);
+  });
+  return paragraph;
+};
 
-  })
-  return paragraph
-}
-
-const generateRepoDetails = (element) => {
+const generateRepoDetails = element => {
   const uList = document.createElement('ul');
   uList.setAttribute('id', 'mlist');
   const liList1 = document.createElement('li');
-  liList1.appendChild(document.createTextNode(`Repository : ${element.name}`))
-  uList.appendChild(liList1)
+  liList1.setAttribute('id', 'liList');
+  liList1.appendChild(document.createTextNode(`Repository : ${element.name}`));
+  uList.appendChild(liList1);
   const liList2 = document.createElement('li');
-  liList2.appendChild(document.createTextNode(`Descriptions: ${element.description}`))
-  uList.appendChild(liList2)
+  liList2.setAttribute('id', 'liList');
+  liList2.appendChild(document.createTextNode(`Descriptions: ${element.description}`));
+  uList.appendChild(liList2);
   const liList3 = document.createElement('li');
-  liList3.appendChild(document.createTextNode(`Forks : ${element.forks}`))
-  uList.appendChild(liList3)
+  liList3.setAttribute('id', 'liList');
+  liList3.appendChild(document.createTextNode(`Forks : ${element.forks}`));
+  uList.appendChild(liList3);
   const liList4 = document.createElement('li');
-  liList4.appendChild(document.createTextNode(`Updated : ${element.updated_at}`))
-  uList.appendChild(liList4)
+  liList4.setAttribute('id', 'liList');
+  liList4.appendChild(document.createTextNode(`Updated : ${element.updated_at}`));
+  uList.appendChild(liList4);
   return uList;
-}
+};
 
 const generateRepoList = async () => {
   const aArray = await getData('https://api.github.com/orgs/foocoding/repos?per_page=100');
@@ -70,39 +70,29 @@ const generateRepoList = async () => {
   details.setAttribute('id', 'details');
   let str;
   uList.setAttribute('id', 'mainList');
-  const firstButton = async (element) => {
-    details.remove();
-    rightDiv.remove();
-    details = await generateRepoDetails(element);
-    rightDiv = await generateContributions(element);
-    root.append(details);
-    root.append(rightDiv);
-
-  }
-  firstButton(myArray[0]);
 
   myArray.forEach(element => {
     const liList = document.createElement('li');
     str = capitalizeFirstLetter(`${element.name}`);
     const btn = document.createElement('button');
     liList.appendChild(btn);
-    btn.appendChild(document.createTextNode(str))
+    btn.appendChild(document.createTextNode(str));
     btn.setAttribute('id', str);
-    uList.appendChild(liList)
+    uList.appendChild(liList);
     leftDiv.appendChild(uList);
-    btn.addEventListener('click', async (element) => {
+    btn.addEventListener('click', async element => {
       details.remove();
       rightDiv.remove();
       details = await generateRepoDetails(element);
-      rightDiv = await generateContributions(element);
+      //rightDiv = await generateContributions(element);
       root.append(details);
       root.append(rightDiv);
     });
-    root.append(leftDiv)
-  })
-}
+    root.append(leftDiv);
+  });
+};
 
-const customizingTheArray = (anArray) => {
+const customizingTheArray = anArray => {
   let myArray = [];
   anArray.map(item => {
     item.name = capitalizeFirstLetter(item.name);
@@ -110,10 +100,10 @@ const customizingTheArray = (anArray) => {
   });
   myArray.sort((a, b) => a.name.localeCompare(b.name));
   return myArray;
-}
+};
 
-const capitalizeFirstLetter = (string) => {
+const capitalizeFirstLetter = string => {
   return string.charAt(0).toUpperCase() + string.slice(1);
-}
+};
 
 generateRepoList();
