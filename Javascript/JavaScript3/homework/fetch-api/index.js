f115ad2bb92e162bcd81e5593f4ff4023ec2641d
Ali Ahdab <aliahdab@gmail.com>
2019-10-05 21:46:18 +0200

    change folders name

--- Javascript/JavaScript3/homework/fetch-api/index.js
+++ Javascript/JavaScript3/homework/fetch-api/index.js
@@ -1,22 +1,20 @@
-'use strict'
+'use strict';
 
-const getData = async (url) => {
+const getData = async url => {
   try {
-    let res = await fetch(url)
+    let res = await fetch(url);
     let data = await res.json();
-    //console.log(data);
-    return data
+    return data;
   } catch (err) {
-    console.log(err)
+    console.log(err);
   }
-}
+};
 
-const generateContributions = async (element) => {
+const generateContributions = async element => {
   const anArray = await getData(element.contributors_url);
   const rightDiv = document.createElement('p');
   rightDiv.setAttribute('id', 'rightDiv');
-  anArray.forEach((ele) => {
-
+  anArray.forEach(ele => {
     const uList = document.createElement('ul');
     uList.setAttribute('id', 'ul');
     const liList = document.createElement('li');
@@ -24,40 +22,39 @@ const generateContributions = async (element) => {
     uList.append(liList);
     const a = document.createElement('a');
     a.href = ele['html_url'];
-    a.textContent = ele.login
-    a.target = '_blank',
-      liList.append(a);
+    a.textContent = ele.login;
+    a.target = '_blank';
+    liList.append(a);
     const img = document.createElement('img');
     img.setAttribute('src', ele.avatar_url);
     img.setAttribute('alt', 'Repo Image');
-    //  a.appendChild(img);
-    liList.append(img)
+    liList.append(img);
     rightDiv.append(uList);
-  })
+  });
   return rightDiv;
-}
+};
 
-const generateDepoDetails = (element) => {
+const generateDepoDetails = element => {
   const uList = document.createElement('ul');
   uList.setAttribute('id', `ulList${element.name}`);
   const liList1 = document.createElement('li');
   liList1.setAttribute('id', 'detailsList');
-  liList1.appendChild(document.createTextNode(`Repository : ${element.name}`))
+  liList1.appendChild(document.createTextNode(`Repository : ${element.name}`));
   uList.appendChild(liList1);
   const liList2 = document.createElement('li');
   liList2.setAttribute('id', 'detailsList');
-  liList2.appendChild(document.createTextNode(`Descriptions: ${element.description}`))
-  uList.appendChild(liList2)
+  liList2.appendChild(document.createTextNode(`Descriptions: ${element.description}`));
+  uList.appendChild(liList2);
   const liList3 = document.createElement('li');
   liList3.setAttribute('id', 'detailsList');
-  liList3.appendChild(document.createTextNode(`Forks : ${element.forks}`))
-  uList.appendChild(liList3)
+  liList3.appendChild(document.createTextNode(`Forks : ${element.forks}`));
+  uList.appendChild(liList3);
   const liList4 = document.createElement('li');
   liList4.setAttribute('id', 'detailsList');
-  liList4.appendChild(document.createTextNode(`Updated : ${element.updated_at}`))
-  uList.appendChild(liList4)
+  liList4.appendChild(document.createTextNode(`Updated : ${element.updated_at}`));
+  uList.appendChild(liList4);
   return uList;
-}
+};
 
 const generateDepoList = async () => {
   const aArray = await getData('https://api.github.com/orgs/foocoding/repos?per_page=100');
@@ -76,9 +73,9 @@ const generateDepoList = async () => {
     str = capitalizeFirstLetter(`${element.name}`);
     const btn = document.createElement('button');
     liList.appendChild(btn);
-    btn.appendChild(document.createTextNode(str))
+    btn.appendChild(document.createTextNode(str));
     btn.setAttribute('id', str);
-    uList.appendChild(liList)
+    uList.appendChild(liList);
     root.appendChild(uList);
 
     btn.addEventListener('click', async () => {
@@ -94,11 +91,11 @@ const generateDepoList = async () => {
   root.append(details);
   rightDiv = await generateContributions(myArray[0]);
   root.append(rightDiv);
-}
+};
 
 generateDepoList();
 
-const customizingTheArray = (anArray) => {
+const customizingTheArray = anArray => {
   let myArray = [];
   anArray.map(item => {
     item.full_name = item.full_name.slice(10);
@@ -109,8 +106,8 @@ const customizingTheArray = (anArray) => {
   myArray.sort((a, b) => a.name.localeCompare(b.name));
 
   return myArray;
-}
+};
 
-const capitalizeFirstLetter = (string) => {
+const capitalizeFirstLetter = string => {
   return string.charAt(0).toUpperCase() + string.slice(1);
-}
+};
